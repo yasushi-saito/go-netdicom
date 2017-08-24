7db4a594dc2528de069d11a81a1b2995cae10f67
Yaz Saito (Dell) <ysaito@grailbio.com>
2017-08-24 15:57:22 -0700

    When sending C-STORE data, remove the metadata part. This is a temp hack.

--- storeclient/storeclient.go
+++ storeclient/storeclient.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"flag"
 	"github.com/yasushi-saito/go-dicom"
 	"github.com/yasushi-saito/go-netdicom"
@@ -10,47 +12,59 @@ import (
 )
 
 var (
-	fileFlag   = flag.String("file", "", "the DICOM file you want to parse")
-	serverFlag = flag.String("server", "", "host:port of the DICOM service provider")
+//	fileFlag   = flag.String("file", "", "the DICOM file you want to parse")
+//	serverFlag = flag.String("server", "", "host:port of the DICOM service provider")
 )
 
 func main() {
 	flag.Parse()
+	if len(flag.Args()) != 2 {
+		log.Fatal("Usage: storeclient <serverhost:port> <file>")
+	}
+	server, inPath := flag.Arg(0), flag.Arg(1)
 
-	if *serverFlag == "" || *fileFlag == "" {
-		log.Fatal("Both --server and --file must be set")
-	}
-	data, err := ioutil.ReadFile(*fileFlag)
+	data, err := ioutil.ReadFile(inPath)
 	if err != nil {
-		log.Fatalf("%s: %v", *fileFlag, err)
-	}
-	file, err := dicom.ParseBytes(data)
-	if err != nil {
-		log.Fatalf("%s: failed to parse as DICOM: %v", *fileFlag, err)
-	}
-	sopInstanceUID, err := file.LookupElement("SOPInstanceUID")
-	if err != nil {
-		log.Fatalf("%s: file does not contain SOPInstanceUID: %v", *fileFlag, err)
+		log.Fatalf("%s: %v", inPath, err)
 	}
 
-	transferSyntaxUID, err := file.LookupElement("TransferSyntaxUID")
-	if err != nil {
-		log.Fatalf("%s: file does not contain TransferSyntaxUID: %v", *fileFlag, err)
+	decoder := dicom.NewDecoder(
+		bytes.NewBuffer(data),
+		int64(len(data)),
+		binary.LittleEndian,
+		dicom.ExplicitVR)
+	meta := dicom.ParseFileHeader(decoder)
+	if decoder.Error() != nil {
+		log.Fatalf("%s: failed to parse as DICOM: %v", inPath, decoder.Error())
 	}
-	sopClassUID, err := file.LookupElement("SOPClassUID")
+	sopInstanceUID, err := dicom.LookupElement(meta, "MediaSOPInstanceUID")
 	if err != nil {
-		log.Fatalf("%s: file does not contain AbstractSyntaxUID: %v", *fileFlag, err)
+		log.Fatalf("%s: file does not contain SOPInstanceUID: %v", inPath, err)
+	}
+
+	transferSyntaxUID, err := dicom.LookupElement(meta, "TransferSyntaxUID")
+	if err != nil {
+		log.Fatalf("%s: file does not contain TransferSyntaxUID: %v", inPath, err)
+	}
+	sopClassUID, err := dicom.LookupElement(meta, "MediaSOPClassUID")
+	if err != nil {
+		log.Fatalf("%s: file does not contain AbstractSyntaxUID: %v", inPath, err)
 	}
 	log.Printf("%s: DICOM transfersyntax:%s, abstractsyntax: %s, sopinstance: %s",
-		*fileFlag, transferSyntaxUID, sopClassUID, sopInstanceUID)
+		inPath, transferSyntaxUID, sopClassUID, sopInstanceUID)
 
 	params := netdicom.NewServiceUserParams(
-		*serverFlag, "dontcare", "testclient", netdicom.StorageClasses,
+		server, "dontcare", "testclient", netdicom.StorageClasses,
 		[]string{dicom.MustGetString(*transferSyntaxUID)})
 	su := netdicom.NewServiceUser(params)
-	err = su.CStore(dicom.MustGetString(*sopClassUID), dicom.MustGetString(*sopInstanceUID), data)
+
+	body := decoder.DecodeBytes(int(decoder.Len()))
+	if decoder.Error() != nil {
+		log.Panic("read")
+	}
+	err = su.CStore(dicom.MustGetString(*sopClassUID), dicom.MustGetString(*sopInstanceUID), body)
 	if err != nil {
-		log.Fatalf("%s: cstore failed: %v", *fileFlag, err)
+		log.Fatalf("%s: cstore failed: %v", inPath, err)
 	}
 	su.Release()
 }
